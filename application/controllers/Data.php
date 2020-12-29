ecb6c7e6a352054d8fab963bfa23689034a8ca11
teguh sabara <teguhsabara16@gmail.com>
2020-12-29 13:04:01 +0700

    controller Data

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -9,7 +9,111 @@ class Data extends CI_Controller {
         // Your own constructor code
     }
     
-    public function sdc()
+    public function cybercop()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function eri()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function sdc()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function ssc()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function tmc()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function intan()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function ais()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function taa()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
+	}
+	public function tarc()
 	{
         $user=$this->session->userdata('user_data');
         $data['js_local'] = 'sdc/dashboard.js';
