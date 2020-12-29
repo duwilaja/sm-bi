aec2626d24415c39a161d1c621e96949c5d2ed63
sahrul rizal <sahrulrizal22@gmail.com>
2020-12-29 10:38:53 +0700

    menmabhkan dashboard SDC

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -6,7 +6,21 @@ class Data extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		// Your own constructor code
+        // Your own constructor code
+    }
+    
+    public function sdc()
+	{
+        $user=$this->session->userdata('user_data');
+        $data['js_local'] = 'sdc/dashboard.js';
+		if(isset($user)){
+			$data['session'] = $user;
+			$this->template->load("data/sdc/dashboard",$data);
+		}else{
+			$retval=array("403","Failed","Please login","error");
+			$data['retval']= $retval;
+			$this->load->view('login',$data);
+		}
 	}
 	
 }
