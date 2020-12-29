ae7942550e1cac7bf550d9a04f21aa59a691e391
sahrul rizal <sahrulrizal22@gmail.com>
2020-12-29 10:03:35 +0700

    Data

---
+++ application/controllers/Data.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Data extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		// Your own constructor code
+	}
+	
+}
