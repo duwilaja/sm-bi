5212673a4e071d518a68cf60aad704cff99e1492
teguh sabara <teguhsabara16@gmail.com>
2021-01-03 19:15:16 +0700

    filter

--- application/views/data/eri/dashboard.php
+++ application/views/data/eri/dashboard.php
@@ -7,48 +7,62 @@
 	}
 </style>
 
-<div class="row">
-							<div class="col-lg-12 col-md-12">
-								<div  class="card">
-									<div class="card-header">
-										<h3 class="card-title">Filter By</h3>
-										<div class="card-options ">
-											<a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
-											<a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="form-group m-0">
-											<!-- <label class="form-label">Date of birth</label> -->
-											<div class="row gutters-xs">
-												<div class="col-5">
-													<input type="date" class="form-control pull-right" id="reservation">
-												</div>
-												<div class="col-3">
-													<select name="polda" class="form-control custom-select">
-														<option value="">Pilih Polda</option>
-														<option value="1">Aceh</option>
-														<option value="2">Sumut</option>
-														<option value="3">Sumbar</option>
-													</select>
-												</div>
-												<div class="col-4">
-													<select name="" class="form-control custom-select">
-														<option value="">Pilih Polres</option>
-														<option value="">ACEH BARAT</option>
-														<option value="">ACEH BARAT DAYA</option>
-														<option value="">SABANG</option>
-													</select>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+
 
 <!--Row-->
 <div class="row">
+<div class="col-md-12">
+            <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <i class="fa fa-search"></i><span class="ml-1">Filter Data</span>
+                </a>
+            </p>
+            <div class="collapse" id="collapseExample">
+                <div class="card">
+                    <form action="javascript:void(0);" method="post" id="filter_karyawan">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <div class="form-group">
+                                        <p>Start Date</p>
+                                        <input class="form-control form-control-sm" type="date" name="f_date_start" id="f_date_start">
+                                    </div>
+                                </div>
+								<div class="col-md-2">
+                                    <div class="form-group">
+                                        <p>End Date</p>
+                                        <input class="form-control form-control-sm" type="date" name="f_date_end" id="f_date_end">
+                                    </div>
+                                </div>
+								<div class="col-md-2">
+                                    <div class="form-group">
+                                        <p>Polda</p>
+                                        <select class="form-control form-control-sm"  name="f_polda" id="f_polda">
+											<option value="">-- Pilih Polda --</option>
+										</select>
+                                    </div>
+                                </div>
+								<div class="col-md-2">
+                                    <div class="form-group">
+                                        <p>Polres</p>
+                                        <select class="form-control form-control-sm"  name="f_polres" id="f_polres">
+											<option value="">-- Pilih Polres --</option>
+										</select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer overflow-hidden">
+                            <div style="float:right;">
+                                <button type="reset" onclick="reset_form()" class="btn btn-warning">Reset</button>
+                                <button type="submit" id="cari" class="btn btn-success" type="submit" >Cari</button>
+                                <!-- <button type="submit" id="cari" class="btn btn-success" type="submit" onclick="lihatDt()">Cari</button> -->
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 	<div class="col-xl-12 col-md-12 col-lg-12">
 		<div class="card">
 			<div class="card-body" style="cursor: all-scroll;">
