37ad0966ca56d5db02e7860bfcfccbd45c72080e
Diva Wardana <divawardana26@gmail.com>
2021-03-19 10:42:15 +0700

    upd detail

--- application/views/data/ssc/detail.php
+++ application/views/data/ssc/detail.php
@@ -26,7 +26,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-6">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-5">
         <div class="card overflow-hidden work-progress">
             <div class="card-header">
                 <h3 class="card-title">Pantauan CCTV</h3>
@@ -43,61 +43,50 @@
         </div>
     </div><!-- COL END -->
 
-    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-6">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-7">
         <div class="row">
             <div class="col-sm-12 col-lg-6 col-md-6 ">
                 <div class="card">
                     <div class="card-body">
-                        <div class="card-order">
-                            <div class="row">
-                                <div class="col">
-                                    <div class=""><b>Kendaraan Hari Ini</b></div>
-                                    <div class="h3 mt-2 mb-2"><b>99,459</b></div>
-                                </div>
-                                <div class="col-auto align-self-center ">
-                                    <div class="feature mt-0 mb-0">
-                                        <i class="fa fa-automobile project bg-primary-transparent text-primary"></i>
-                                    </div>
-                                </div>
+                        <div class="d-flex clearfix">
+                            <div class="text-left ">
+                                <p class="card-text mb-1"><b>Total Kendaraan</b></p>
+                                <h2 class="mb-1 font-weight-semibold mainvalue">80,956</h2>
+                                <p class="mb-0 text-muted"><span class="mb-0 text-success fs-13 "><i class="fe fe-arrow-up "></i> 22%</span> vs hari sebelumnya</p>
                             </div>
-                            <span class="mb-0 text-muted"><span class="text-success"><i class="fa fa-caret-up  mr-1"></i> 0.7%</span> vs hari sebelumnya</span>
+                            <div class="ml-auto">
+                                <span class="bg-primary-transparent icon-service text-primary ">
+                                    <i class="fa fa-automobile"></i>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-sm-12 col-lg-6 col-md-6">
-                <div class="card text-center">
-                    <div class="card-body ">
-                        <div class="feature widget-2 text-center mt-0 mb-3">
-                            <i class="ti-stats-up project bg-success-transparent mx-auto text-success "></i>
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex clearfix">
+                            <div class="text-left ">
+                                <p class="card-text mb-1"><b>Kendaraan Hari Ini</b></p>
+                                <h2 class="mb-1 font-weight-semibold mainvalue">99,459</h2>
+                                <p class="mb-0 text-muted"><span class="mb-0 text-success fs-13 "><i class="fe fe-arrow-up "></i> 22%</span> vs hari sebelumnya</p>
+                            </div>
+                            <div class="ml-auto">
+                                <span class="bg-success-transparent icon-service text-success">
+                                    <i class="ti-stats-up text-success"></i>
+                                </span>
+                            </div>
                         </div>
-                        <h6 class="mb-1 text-muted">Total Kendaraan</h6>
-                        <h3 class="font-weight-semibold">20,100,900</h3>
                     </div>
                 </div>
-            </div>
+            </div>  
             <div class="col-sm-12 col-lg-12 col-md-12 col-xl-12">
                 <div class="card">
                     <div class="card-body">
                         <h3 class="card-title">Status Volume Kendaraan</h3>
-                        <h3 style="margin-bottom:2px !important;"><b>Padat</b></h3>
+                        <h3 style="margin-bottom:2px !important;"><b>Padat</b> <span class="sparkline_bar float-right"></span></h3>
                         <p class="text-muted">Lonjakan peningkatan jumlah kendaraan perdetik</p>
-                        <div class="progress progress-md h-4 mt-5">
-                            <!-- <div class="progress-bar bg-success w-40"></div>
-                            <div class="progress-bar bg-warning w-50"></div> -->
-                            <div class="progress-bar bg-danger w-80"></div>
-                        </div>
-                        <div class="row mt-4">
-                            <div class="col-md-4">
-                                <h6 class="mb-1"><span class="dot-label bg-success"></span>Lenggang</h6>
-                            </div>
-                            <div class="col-md-4  mt-4 mt-xl-0">
-                                <h6 class="mb-1"><span class="dot-label bg-warning"></span>Ramai Lancar</h6>
-                            </div>
-                            <div class="col-md-4 mt-4 mt-xl-0">
-                                <h6 class="mb-1"><span class="dot-label bg-danger"></span>Padat</h6>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
