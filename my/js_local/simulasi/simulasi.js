da6b25bc710fdee99488c37608f2140f4b8ac514
Diva Wardana <divawardana26@gmail.com>
2021-03-26 16:50:33 +0700

    upd simulasi

--- my/js_local/simulasi/simulasi.js
+++ my/js_local/simulasi/simulasi.js
@@ -5,19 +5,20 @@ function initMap() {
     center: { lat: -7.5592506525457, lng: 110.8228297937785 },
     zoom: 15,
     mapTypeId: "satellite",
-    heading: 90,
+    // heading: 90,
     tilt: 45,
+    disableDefaultUI: true
   });
 }
 
-function rotate90() {
-  const heading = map.getHeading() || 0;
-  map.setHeading(heading + 90);
-}
+// function rotate90() {
+//   const heading = map.getHeading() || 0;
+//   map.setHeading(heading + 90);
+// }
 
-function autoRotate() {
-  // Determine if we're showing aerial imagery.
-  if (map.getTilt() !== 0) {
-    window.setInterval(rotate90, 3000);
-  }
-}
+// function autoRotate() {
+//   // Determine if we're showing aerial imagery.
+//   if (map.getTilt() !== 0) {
+//     window.setInterval(rotate90, 3000);
+//   }
+// }
