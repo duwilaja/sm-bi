9a1e9c871859b6cd39c04d9f384bd19dee909dc4
sahrul rizal <sahrulrizal22@gmail.com>
2021-05-07 23:39:48 +0700

    fix

--- application/models/MIndicar.php
+++ application/models/MIndicar.php
@@ -1,7 +1,7 @@
 
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Mindicar extends CI_Model {
+class MIndicar extends CI_Model {
 
     private $t = '';
     public $see = '*';
